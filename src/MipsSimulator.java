9b794ec44c2a2ca6c20cc0b3ceb27e454817f4da
sfishe10 <sfishe10@calpoly.edu>
2023-05-25 13:18:18 -0700

    ghr working

--- src/MipsSimulator.java
+++ src/MipsSimulator.java
@@ -1,24 +1,14 @@
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.text.DecimalFormat;
+import java.util.*;
 
 public class MipsSimulator {
     private final int[] datamem = new int[8192];
     private int pc = 0;
-    private int ghr_size;
-    private int correct;
-    private int incorrect;
-    private int prev_prediction;
-    private boolean branchTaken;
-  //  private int[] bp_table = new int[ghr_size];
-    private int[] counters = new int[2];
+    private final int ghr_size;
     private int ghr = 0;
-    // if branch is not taken, left shift
-    // if branch is taken, left shift AND set lsb to 1
-    // in both cases, set bit to the left of ghr size to 0
     private int tot_predictions = 0;
     private int acc_predictions = 0;
-    private int[] predictors = new int[(int) Math.pow(2, ghr_size)];
+    private final int[] predictors;
     private final LinkedHashMap<String, Integer> regs = new LinkedHashMap<>();
     private final ArrayList<Instruction> insts;
 
@@ -31,42 +21,44 @@ public class MipsSimulator {
 
         this.insts = insts;
         this.ghr_size = ghr_size;
+        int pred_size = (int) Math.pow(2, ghr_size);
+        predictors = new int[pred_size];
 
-    //    for (int i = 0; i < ghr_size; i++) {
-     //       bp_table[i] = 0;
-     //   }
-        for (int i=0; i < 2; i++) {
-            predictors[i] = 0;
-        }
+        Arrays.fill(predictors, 0);
     }
 
     public void setReg(String reg, int num) {
-        if (reg != "$0" && reg != "$zero") {
+        if (!Objects.equals(reg, "$0") && !Objects.equals(reg, "$zero")) {
             regs.replace(reg, num);
         }
     }
 
     public void b() {
-        System.out.println("accuracy " + ((acc_predictions/tot_predictions)*100) + "% (" +
+        DecimalFormat df = new DecimalFormat("0.00");
+        String acc = df.format(((float)acc_predictions/(float)tot_predictions) * 100);
+        System.out.println("\n\naccuracy " + acc + "% (" +
                 acc_predictions + " correct predictions, " + tot_predictions + " predictions)");
     }
 
     public static void h() {
-        System.out.println("\n\nh = show help\n" +
-                "d = dump register state\n" +
-                "s = single step through the program (i.e. execute 1 instruction and stop)\n" +
-                "s num = step through num instructions of the program\n" +
-                "r = run until the program ends\n" +
-                "m num1 num2 = display data memory from location num1 to num2\n" +
-                "c = clear all registers, memory, and the program counter to 0\n" +
-                "q = exit the program");
+        System.out.println("""
+
+
+                h = show help
+                d = dump register state
+                s = single step through the program (i.e. execute 1 instruction and stop)
+                s num = step through num instructions of the program
+                r = run until the program ends
+                m num1 num2 = display data memory from location num1 to num2
+                c = clear all registers, memory, and the program counter to 0
+                q = exit the program""");
     }
     public void d() {
         System.out.println("\n\npc = " + pc);
         int count = 0;
 
         for ( Map.Entry<String, Integer> reg: regs.entrySet()) {
-            if (reg.getKey() == "$zero") {
+            if (Objects.equals(reg.getKey(), "$zero")) {
                 continue;
             }
             System.out.print(reg.getKey() + " = " + reg.getValue());
@@ -86,7 +78,7 @@ public class MipsSimulator {
     public void s() {
         if (pc < insts.size()) {
             Instruction inst = insts.get(pc);
-            branchTaken = false;
+            boolean branchTaken = false;
             switch (inst.getId()) {
                 case ("add"):
                     int sum = regs.get(((R)inst).getRs()) + regs.get(((R)inst).getRt());
@@ -143,22 +135,20 @@ public class MipsSimulator {
                     pc++;
                     break;
                 case ("beq"):
-                    if (regs.get(((I)inst).getRs()) == regs.get(((I)inst).getRt())){
+                    if (Objects.equals(regs.get(((I) inst).getRs()), regs.get(((I) inst).getRt()))){
                         pc +=  1 + ((I)inst).getImm();
                         branchTaken = true;
-                        break;
                     }
                     else {
-                        branchTaken = false;
                         pc++;
                     }
+                    break;
                 case ("bne"):
-                    if (regs.get(((I)inst).getRs()) != regs.get(((I)inst).getRt())){
+                    if (!Objects.equals(regs.get(((I) inst).getRs()), regs.get(((I) inst).getRt()))){
                         pc +=  1 + ((I)inst).getImm();
-                        branchTaken = false;
+                        branchTaken = true;
                     }
                     else {
-                        branchTaken = true;
                         pc++;
                     }
                     break;
@@ -173,38 +163,40 @@ public class MipsSimulator {
                     break;
             }
 
-        }
-        prev_prediction = predictors[ghr];
-        if (branchTaken == true && (prev_prediction == 2 || prev_prediction == 3)) {
-            correct++;
-            //update bp_table
-            if (prev_prediction != 3){
-                predictors[ghr] = prev_prediction + 1;
-            }
-        }
-        else  if (branchTaken == false && (prev_prediction == 0 || prev_prediction == 1)) {
-            correct++;
-            if (prev_prediction != 3){
-                predictors[ghr] = prev_prediction + 1;
+            // determine if prediction was accurate
+            if (Objects.equals(inst.getId(), "beq") || Objects.equals(inst.getId(), "bne")) {
+                int prev_prediction = predictors[ghr];
+                if (branchTaken) {
+                    if (prev_prediction == 2 || prev_prediction == 3) {
+                        acc_predictions++;
+                    }
+                    tot_predictions++;
+
+                    // update predictors
+                    if (prev_prediction != 3) {
+                        predictors[ghr] = prev_prediction + 1;
+                    }
+
+                    // update ghr
+                    ghr = ((ghr << 1) & (~(int)Math.pow(2, ghr_size))) + 1;
+                }
+                else {
+                    if (prev_prediction == 0 || prev_prediction == 1) {
+                        acc_predictions++;
+                    }
+                    tot_predictions++;
+
+                    // update predictors
+                    if (prev_prediction != 0){
+                        predictors[ghr] = prev_prediction - 1;
+                    }
+
+                    // update ghr
+                    ghr = (ghr << 1) & (~(int)Math.pow(2, ghr_size));
+                }
             }
 
         }
-        else  if (branchTaken == true && (prev_prediction == 0 || prev_prediction == 1)) {
-            incorrect++;
-            if (prev_prediction != 0){
-                predictors[ghr] = prev_prediction - 1;
-            }
-            
-        }
-        else  if (branchTaken == false && (prev_prediction == 2 || prev_prediction == 3)) {
-            incorrect++;
-            if (prev_prediction != 0){
-                predictors[ghr] = prev_prediction - 1;
-            }
-            
-        }
-        
-        
 
 
     }
